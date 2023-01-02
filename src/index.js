4d600b136dc3dcb847cfdcfab953ec21da994c7e
MihajloGerbach <108993799+MihajloGerbach@users.noreply.github.com>
2023-01-02 18:51:09 +0300

    дз-8

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryCardEl(galleres) {
+  return galleres
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"/>
+            </a>
+           </div> `;
+    })
+    .join('');
+}
+const galleryCard = createGalleryCardEl(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryCard);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
