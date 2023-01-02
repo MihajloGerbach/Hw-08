38c3ca0ff4582193c65e72dd806abd4287f6bb27
MihajloGerbach <108993799+MihajloGerbach@users.noreply.github.com>
2023-01-02 19:38:14 +0300

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+function createGalleryCardEl(galleres) {
+  return galleres
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"/>
+            </a>
+           </div> `;
+    })
+    .join('');
+}
+const galleryCard = createGalleryCardEl(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryCard);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 // Change code below this line
 
 console.log(galleryItems);
